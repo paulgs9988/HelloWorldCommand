a41e048df5e03f14b45993ca8b8911559e346331
Paul Sullivan <paulsullivan@Pauls-MacBook-Air.local>
2020-08-19 18:20:31 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.sql.SQLOutput;
+
+import static java.lang.System.out;
+
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
